ea1e6e61cc1168d3563f8bd449b56a47f7106ee9
Zithrexuz_ <62383382+Zithrexuz@users.noreply.github.com>
2023-12-02 01:08:37 +0100

    Update Component1.js

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,7 +1,7 @@
-import React from 'react';
+//import React from 'react';
 
-function Component1() {
-  return <h1>This is Component 1</h1>;
-}
+//function Component1() {
+//  return <h1>This is Component 1</h1>;
+//}
 
-export default Component1;
+//export default Component1;
