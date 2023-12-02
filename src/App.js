f2cc9b83b9a52900b7e21b2b382e23c89ef8b1a5
Zithrexuz <Zithrexuz@users.noreply.github.com>
2023-12-02 02:21:59 +0100

    modified: src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Component1 from './components/Component1';
-import Component2 from './components/Component2';
+//import Component2 from './components/Component2';
 
 function App() {
   return (
     <div className="App">
       <Component1 />
-      <Component2 />
     </div>
   );
 }
