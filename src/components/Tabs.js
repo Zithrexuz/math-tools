3bd842be52a3490f87f2e3b5c031ffde62e16c89
Zithrexuz <Zithrexuz@users.noreply.github.com>
2023-12-03 19:25:25 +0100

    Removed commented code

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,19 +1,3 @@
-/*
-import React from 'react';
-
-function Tabs({ onChange }) {
-  return (
-    <div>
-      <button onClick={() => onChange('home')}>Home</button>
-      <button onClick={() => onChange('cardGames')}>Card Games</button>
-      {/* Add your other tabs here //}
-    </div>
-  );
-}
-
-export default Tabs;
-*/
-
 import React from 'react';
 import styled from 'styled-components';
 
