d682222ba40fbd939e0bd4e933093b933d95f0db
Zithrexuz <Zithrexuz@users.noreply.github.com>
2023-12-03 02:49:05 +0100

    Input

--- src/components/CardGames.js
+++ src/components/CardGames.js
@@ -32,6 +32,38 @@ export default CardGames;
 
 //import React, { useState } from 'react';
 //import { useEffect } from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { MdDelete } from 'react-icons/md';
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const CreateButton = styled.button`
+  margin-bottom: 20px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #4caf50;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+const PlayerInput = styled.input`
+  margin-bottom: 10px;
+`;
+
+const DeleteButton = styled(MdDelete)`
+  margin-left: 10px;
+  cursor: pointer;
+`;
+
 function CardGames({ players, onPlayerNameChange, onCreatePlayers }) {
   const [playerCount, setPlayerCount] = useState(0);
   const [gameStarted, setGameStarted] = useState(false);
