d588885c434805c0bba4f43ae57efa20bfb6ce3c
Zithrexuz <Zithrexuz@users.noreply.github.com>
2023-12-03 01:18:22 +0100

    Debugging input name

--- src/components/CardGames.js
+++ src/components/CardGames.js
@@ -104,7 +104,7 @@ function CardGames({ players, onPlayerCountChange, onPlayerNameChange, onCreateG
       {gameStarted && (
         <>
           <input type="number" onChange={onPlayerCountChange} placeholder="Enter number of players" />
-          {players.map((player, index) => (
+          {gameCreated && players.map((player, index) => (
             <div key={index}>
               <input type="text" value={player.name} onChange={(event) => onPlayerNameChange(index, event)} placeholder={`Enter name of player ${index + 1}`} /> {/* <PlayerInput type="text" value={player.name} onChange={(event) => onPlayerNameChange(index, event)} placeholder={`Enter name of player ${index + 1}`} /> */}
               <button size={20} onClick={() => onPlayerNameChange(index, '')} /> {/* <DeleteButton size={20} onClick={() => onPlayerNameChange(index, '')} /> */}
