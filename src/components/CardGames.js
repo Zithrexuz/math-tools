855b35720e3f0911c166542c66e5f6e9a0c4d82b
Zithrexuz <Zithrexuz@users.noreply.github.com>
2023-12-08 22:32:29 +0100

    Fixed an error with the styling, forgot to define my AppContainer.

--- src/components/CardGames.js
+++ src/components/CardGames.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { MdDelete } from 'react-icons/md';
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+`;
+
 const Title = styled.h1`
   margin-bottom: 20px;
   color: #fff;
