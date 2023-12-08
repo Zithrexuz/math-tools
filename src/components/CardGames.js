973bebaf2ce7e4f1bf37d604cf095def6e16224d
Zithrexuz <Zithrexuz@users.noreply.github.com>
2023-12-08 22:38:35 +0100

    Removed vheight from AppContainer style in CardGames

--- src/components/CardGames.js
+++ src/components/CardGames.js
@@ -7,7 +7,7 @@ const AppContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  //height: 100vh;
   background-color: #202124;
 `;
 
